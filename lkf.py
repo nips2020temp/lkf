33f2571dba83819a5fe00dde793713e7bf6205b1
Anand <anand@alphasheets.com>
2020-05-08 21:45:24 -0400

    changes

--- lkf.py
+++ lkf.py
@@ -9,7 +9,7 @@ from typing import Callable
 import numpy as np
 
 class LKF(LSProcess):
-	def __init__(self, x0: np.ndarray, F: Callable, H: np.ndarray, Q: np.ndarray, R: np.ndarray, dt: float, tau: float):
+	def __init__(self, x0: np.ndarray, F: Callable, H: np.ndarray, Q: np.ndarray, R: np.ndarray, dt: float, tau: float = float('inf')):
 		self.F = F
 		self.H = H
 		self.Q = Q
@@ -18,15 +18,27 @@ class LKF(LSProcess):
 		self.tau = tau
 		self.ndim = x0.shape[0]
 		self.err_hist = []
+		self.eta_t = np.zeros((self.ndim, self.ndim)) # temp var..
 
-		def f(t, state, z_t, err_t, err_tau, F_t):
+		def f(t, state, z_t, err_hist, F_t):
 			state = state.reshape(self.ode_shape)
 			x_t, P_t, eta_t = state[:, :1], state[:, 1:3], state[:, 3:]
-			z_t, err_t, err_tau = z_t[:, np.newaxis], err_t[:, np.newaxis], err_tau[:, np.newaxis]
-			H_inv = np.linalg.inv(self.H)
-			P_inv = np.linalg.inv(P_t)
-			eta_t = H_inv@(err_t@err_t.T - err_tau@err_tau.T)@H_inv.T@P_inv / (2*tau)
-			# print(np.linalg.norm(eta_t))
+			z_t = z_t[:, np.newaxis]
+			if t > self.tau: 
+				err_t, err_tau = err_hist[-1][:,np.newaxis], err_hist[0][:,np.newaxis]
+				tau_n = len(err_hist)
+				tau_h = tau_n * self.dt
+				H_inv = np.linalg.inv(self.H)
+				P_inv = np.linalg.inv(P_t)
+				E_zz = (err_t@err_t.T - err_tau@err_tau.T) / tau_h
+				# E_z1 = (err_t - err_tau) / tau_h
+				# E_z2 = sum(err_hist)[:, np.newaxis] / tau_n
+				# eta_t = H_inv@(E_zz - E_z1@E_z2.T - E_z2@E_z1.T)@H_inv.T@P_inv / 2
+				eta_t = H_inv@E_zz@H_inv.T@P_inv / 2
+				eta_t = np.clip(eta_t, a_min=-1, a_max=1)
+				self.eta_t = eta_t # TODO fix hack
+			else:
+				eta_t = np.zeros((self.ndim, self.ndim)) # TODO warmup case?
 			F_est = F_t - eta_t
 			K_t = P_t@self.H@np.linalg.inv(self.R)
 			d_x = F_est@x_t + K_t@(z_t - self.H@x_t)
@@ -50,19 +62,14 @@ class LKF(LSProcess):
 
 	def __call__(self, z_t: np.ndarray):
 		''' Observe through filter ''' 
-		if self.t > self.tau:
-			err_t, err_tau = self.err_hist[-2], self.err_hist[0]
-		else:
-			err_t, err_tau = np.zeros(self.ndim), np.zeros(self.ndim)
-		self.r.set_f_params(z_t, err_t, err_tau, self.F(self.t))
+		self.r.set_f_params(z_t, self.err_hist, self.F(self.t))
 		self.r.integrate(self.t + self.dt)
 		x_t = np.squeeze(self.r.y.reshape(self.ode_shape)[:, :1])
 		err_t = z_t - x_t@self.H.T
-		d_eta_t = self.r.dy.reshape(self.ode_shape)[:, 3:]
 		self.err_hist.append(err_t)
 		if self.t > self.tau:
 			self.err_hist = self.err_hist[1:]
-		return x_t.copy(), err_t, d_eta_t # x_t variable gets reused somewhere...
+		return x_t.copy(), err_t, self.eta_t # x_t variable gets reused somewhere...
 
 	@property
 	def ode_shape(self):
@@ -78,12 +85,11 @@ if __name__ == '__main__':
 	set_seed(9001)
 
 	dt = 0.001
-	tau = 0.1
-	n = 30000
+	n = 50000
 	z = Oscillator(dt, 0.0, 1.0)
 	eta = np.random.normal(0.0, 0.01, (2, 2))
 	F_hat = lambda t: z.F(t) + eta
-	f = LKF(z.x0, F_hat, z.H, z.Q, z.R, dt, tau)
+	f = LKF(z.x0, F_hat, z.H, z.Q, z.R, dt, tau=0.25)
 	hist_t = []
 	hist_z = []
 	hist_x = []
@@ -91,12 +97,12 @@ if __name__ == '__main__':
 	hist_eta = []
 	for _ in range(n):
 		z_t = z()
-		x_t, err_t, d_eta_t = f(z_t)
+		x_t, err_t, eta_t = f(z_t)
 		hist_z.append(z_t)
 		hist_t.append(z.t)
 		hist_x.append(x_t) 
 		hist_err.append(err_t)
-		hist_eta.append(np.linalg.norm(d_eta_t)) # infinitesimal variation
+		hist_eta.append(np.linalg.norm(eta_t)) # infinitesimal variation
 	hist_t = np.array(hist_t)
 	hist_z = np.array(hist_z)
 	hist_x = np.array(hist_x)
